c520d87c085a5c675cfb9abf8691bfd2fe9499f9
momonisizawa <66298739+momonisizawa@users.noreply.github.com>
2020-06-16 23:36:42 +0900

    chatapp.demo
    チャットアプリ

---
+++ .main.js
@@ -0,0 +1,102 @@
+(() => {
+  "use strict";
+
+  const firebaseConfig = {
+    apiKey: "AIzaSyCmxn5zaXp08LIz1ngmWxw3qZBE46GR3b0",
+    authDomain: "myfirebasechatapp-b0700.firebaseapp.com",
+    databaseURL: "https://myfirebasechatapp-b0700.firebaseio.com",
+    projectId: "myfirebasechatapp-b0700",
+    storageBucket: "myfirebasechatapp-b0700.appspot.com",
+    messagingSenderId: "187430108967",
+    appId: "1:187430108967:web:17c0445ec9e00643233302",
+    measurementId: "G-FM4SN7DZ10",
+  };
+  firebase.initializeApp(firebaseConfig);
+
+  const db = firebase.firestore();
+  db.settings({
+    timestampsInSnapshots: true,
+  });
+  const collection = db.collection("messages");
+
+  const auth = firebase.auth();
+  let me = null;
+
+  const message = document.getElementById("message");
+  const form = document.querySelector("form");
+  const messages = document.getElementById("messages");
+  const login = document.getElementById("login");
+  const logout = document.getElementById("logout");
+
+  login.addEventListener("click", () => {
+    auth.signInAnonymously();
+  });
+
+  logout.addEventListener("click", () => {
+    auth.signOut();
+  });
+
+  auth.onAuthStateChanged((user) => {
+    if (user) {
+      me = user;
+
+      while (messages.firstChild) {
+        messages.removeChild(messages.firstChild);
+      }
+
+      collection.orderBy("created").onSnapshot(
+        (snapshot) => {
+          snapshot.docChanges().forEach((change) => {
+            if (change.type === "added") {
+              const li = document.createElement("li");
+              const d = change.doc.data();
+              li.textContent = d.uid.substr(0, 8) + ": " + d.message;
+              messages.appendChild(li);
+            }
+          });
+        },
+        (error) => {}
+      );
+      console.log(`Logged in as: ${user.uid}`);
+      login.classList.add("hidden");
+      [logout, form, messages].forEach((el) => {
+        el.classList.remove("hidden");
+      });
+      message.focus();
+      return;
+    }
+    me = null;
+    console.log("Nobody is logged in");
+    login.classList.remove("hidden");
+    // form.classList.remove("hidden");
+    [logout, form, messages].forEach((el) => {
+      el.classList.add("hidden");
+    });
+  });
+
+  form.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    const val = message.value.trim();
+    if (val === "") {
+      return;
+    }
+
+    message.value = "";
+    message.focus();
+
+    collection
+      .add({
+        message: val,
+        created: firebase.firestore.FieldValue.serverTimestamp(),
+        uid: me ? me.uid : "nobody",
+      })
+      .then((doc) => {
+        console.log(`${doc.id} added!`);
+      })
+      .catch((error) => {
+        console.log("document add error!");
+        console.log(error);
+      });
+  });
+})();
